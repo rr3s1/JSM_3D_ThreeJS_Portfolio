64fbfb8e4938f51742b737a46b752ba91a66cfa1
aessar <raressilviulazar@gmail.com>
2024-11-01 15:30:52 +0000

    Footer section done

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -33,10 +33,19 @@ const Contact = () => {
                 }, '__EmailjsPUBLICKEY__')
             setLoading(false);
             alert('Thank you. I will get back to you as soon as possible');
+            setForm({
+                name: '',
+                email: '',
+                message: '',
+            });
         } catch (error) {
+
             setLoading(false);
+
             console.log(error);
+
             alert('Something went wrong. Please try again later')
+
 
         }
 
